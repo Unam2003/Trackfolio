5fd75179722d1999d8a65f091390ff4019fd91e1
Emanuele Piemonte <epiemonte1@gmail.com>
2026-05-10 18:05:09 +0200

    Initial commit

---
+++ src/main/java/emanuelepiemonte/Trackfolio/TrackfolioApplication.java
@@ -0,0 +1,13 @@
+package emanuelepiemonte.Trackfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emanuelepiemonte/Trackfolio/TrackfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package emanuelepiemonte.Trackfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

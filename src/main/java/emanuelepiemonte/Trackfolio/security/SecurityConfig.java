52b9c52f9ee637c296b2936951e2423b3d4362cd
Emanuele Piemonte <epiemonte1@gmail.com>
2026-06-01 00:27:52 +0200

    fix configuration origins

--- src/main/java/emanuelepiemonte/Trackfolio/security/SecurityConfig.java
+++ src/main/java/emanuelepiemonte/Trackfolio/security/SecurityConfig.java
@@ -52,9 +52,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://mywonderfulfe.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://trackfoliomanu.netlify.app"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
